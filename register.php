e83e214838f81ec9cb9a5cf76266a4254c23755c
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-05-13 08:27:54 +0800

    PHP AJAX Login and Register

---
+++ register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+  <script
+  src="http://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript">
+  $(document).ready(function() {
+    $('#btnsubmit').click(function() {
+      $.ajax({
+        type: "POST",
+        url: 'php/register.php',
+        data: {
+          firstname: $("#firstname").val(),
+          username: $("#lastname").val(),
+          lastname: $("#username").val(),
+          password: $("#password").val()
+        }
+      });
+      window.location.replace('index.php');
+    });
+  });
+  </script>
+</head>
+<body>	
+  <h2>Register</h2>
+  <a href="index.php">Home</a>
+
+  <form>
+   First Name: <input type="text" name="firstname" id="firstname"> <br>
+   Last Name: <input type="text" name="lastname" id="lastname"> <br>
+   Username: <input type="text" name="username" id="username"> <br>
+   Password: <input type="password" name="password" id="password"> <br>
+  </form>
+  <button id="btnsubmit">Submit</button>
+</body>
+</html>
